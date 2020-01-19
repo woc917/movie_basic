9facb60c4337a8a9fbfcda462553ef88a97297a9
woc917 <woc917@hotmail.com>
2020-01-19 19:16:06 +0900

    삼항연산 활용

--- src/App.js
+++ src/App.js
@@ -4,51 +4,25 @@ import './App.css'
 
 export default class App extends Component {
 
-// 라이프 사이클
+  state = {
+    isLoading: true,
+    movies: []
+  }
 
-// 상태선언
-state = {
-  count: 0
-};
-
-// 함수선언
-add = () => {
-  this.setState(current => ({count: current.count + 1}));
-}
-
-minus = () => {
-  this.setState(current => ({count: current.count - 1 }));
-}
-
-componentDidMount() {
-  console.log("componentDidMount");
-}
-
-componentDidUpdate() {
-  console.log("componentDidUpdate");
-}
-
-componentWillMount() {
-  console.log("componentWillMount");
-}
-
-componentDidCatch() {
-  console.log("componentDidCatch");
-}
-
-componentWillUpdate() {
-  console.log("componentWillUpdate");
-}
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading:false });
+    }, 6000);
+  }
 
   render() {
 
-    console.log("rendering");
+    const { isLoading } = this.state 
 
     return (
       <div className="App">
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+
+        {isLoading ? "Loading..." : "We are ready"}
         
       </div>
     )
