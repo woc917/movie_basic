f3248cc047e1be395802c718b38fea2d46433496
woc917 <woc917@hotmail.com>
2020-01-19 18:43:26 +0900

    add proptypes

--- src/chicken.js
+++ src/chicken.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Chicken({favourite, imgsrc}) {
+function Chicken({favourite, imgsrc, rating}) {
     return (
         <div>
             <h3>{favourite}</h3>
-            <img src={imgsrc} />
+            <h5>{rating} / 5</h5>
+            <img src={imgsrc} alt={favourite} />
         </div>
     );
 }
+
+Chicken.propTypes = {
+    favourite: PropTypes.string.isRequired, // 속성값(스트링)지정, 필수값으로
+    imgsrc: PropTypes.string.isRequired,
+    rating: PropTypes.number // 속성값(번호)지정, 필수값X
+}
+
 
 export default Chicken;
