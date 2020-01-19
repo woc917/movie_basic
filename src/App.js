117ee29dffc878823a8229a860fca8b22b215e17
woc917 <woc917@hotmail.com>
2020-01-19 18:57:12 +0900

    state, function

--- src/App.js
+++ src/App.js
@@ -1,58 +1,31 @@
 import React, { Component } from 'react'
 import './App.css'
-import Chicken from './chicken'
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4.9 
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.7 
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 3.2 
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.0
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 2.7
-  }
-];
 
 export default class App extends Component {
 
 // 라이프 사이클
 
-// 함수선언
 // 상태선언
+state = {
+  count: 0
+};
+
+// 함수선언
+add = () => {
+  console.log("add");
+}
+
+minus = () => {
+  console.log("minus");
+}
 
   render() {
     return (
       <div className="App">
-        {foodILike.map(food => (
-          <Chicken key={food.id} favourite={food.name} imgsrc={food.image} rating={food.rating} />
-        ))}
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
         
       </div>
     )
